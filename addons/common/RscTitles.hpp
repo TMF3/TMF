453e5873ce6a1e5292f2886556806155965fd9aa
Sebastian Broberg <headswe92@gmail.com>
2016-10-10 17:40:02 +0200

    Fix tabs.

--- addons/common/RscTitles.hpp
+++ addons/common/RscTitles.hpp
@@ -3,58 +3,58 @@ class RscPicture;
 class RscText;
 class RscTitles
 {
-	class GVAR(endSplash)
-	{
-		idd = 920528;
-		duration = 99999999999;
+    class GVAR(endSplash)
+    {
+        idd = 920528;
+        duration = 99999999999;
         onLoad = "uiNamespace setVariable [""tmf_common_endSplash"", _this select 0]";
         class controls {
             class Background: IGUIBack
             {
                 idc = TMF_ENDMISSION_BACK;
-            	x = 0 * safezoneW + safezoneX;
-            	y = 0 * safezoneH + safezoneY;
-            	w = 1 * safezoneW;
-            	h = 1 * safezoneH;
+                x = 0 * safezoneW + safezoneX;
+                y = 0 * safezoneH + safezoneY;
+                w = 1 * safezoneW;
+                h = 1 * safezoneH;
                 colorBackground[] = {0,0,0,0.7};
             };
             class Picture: RscPicture
             {
-            	idc = TMF_ENDMISSION_PICTURE;
-            	text = "#(argb,8,8,3)color(1,1,1,0)";
-            	x = 0.3 * safezoneW + safezoneX;
-            	y = 0.2 * safezoneH + safezoneY;
-            	w = 0.4 * safezoneW;
-            	h = 0.4 * safezoneH;
+                idc = TMF_ENDMISSION_PICTURE;
+                text = "#(argb,8,8,3)color(1,1,1,0)";
+                x = 0.3 * safezoneW + safezoneX;
+                y = 0.2 * safezoneH + safezoneY;
+                w = 0.4 * safezoneW;
+                h = 0.4 * safezoneH;
             };
             class Header: RscText
             {
-            	idc = TMF_ENDMISSION_HEADER;
-            	text = "VICTORY"; //--- ToDo: Localize;
+                idc = TMF_ENDMISSION_HEADER;
+                text = "VICTORY"; //--- ToDo: Localize;
                 style = ST_CENTER;
-            	x = 0 * safezoneW + safezoneX;
-            	y = 0 * safezoneH + safezoneY;
-            	w = 1 * safezoneW;
-            	h = 0.8 * safezoneH;
+                x = 0 * safezoneW + safezoneX;
+                y = 0 * safezoneH + safezoneY;
+                w = 1 * safezoneW;
+                h = 0.8 * safezoneH;
                 color[] = {1,1,1,1};
-            	sizeEx = 0.2 *safeZoneH;
+                sizeEx = 0.2 *safeZoneH;
                 font = "PuristaBold";
                 shadow = 0;
             };
             class Subtitle: RscText
             {
-            	idc = TMF_ENDMISSION_SUBTILE;
+                idc = TMF_ENDMISSION_SUBTILE;
                 style = ST_MULTI + ST_NO_RECT + ST_CENTER;
-            	text = "THE MASIF VALLEY REMAINS IN HEAD CONTROL"; //--- ToDo: Localize;
-            	x = 0 * safezoneW + safezoneX;
-            	y = 0.5 * safezoneH + safezoneY;
-            	w = 1 * safezoneW;
-            	h = 0.2 * safezoneH;
+                text = "THE MASIF VALLEY REMAINS IN HEAD CONTROL"; //--- ToDo: Localize;
+                x = 0 * safezoneW + safezoneX;
+                y = 0.5 * safezoneH + safezoneY;
+                w = 1 * safezoneW;
+                h = 0.2 * safezoneH;
                 color[] = {1,1,1,1};
                 shadow = 0;
-            	sizeEx = 0.025* safezoneH;
+                sizeEx = 0.025* safezoneH;
                 font = "PuristaMedium";
             };
         };
-	}
+    }
 };
