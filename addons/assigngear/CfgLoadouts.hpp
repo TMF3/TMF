3037869543aef0202f80c4877de9d0c3594263bb
Bear <beaar@users.noreply.github.com>
2016-10-19 20:47:57 +0200

    Capitalization (#31)
    Bear is a capitalist hero and uppers some lowers.

--- addons/assigngear/CfgLoadouts.hpp
+++ addons/assigngear/CfgLoadouts.hpp
@@ -3,44 +3,44 @@ class cfgLoadouts
     class BLU_F
     {
         displayName = "NATO";
-        category = "vanilla";
+        category = "Vanilla";
         #include "loadouts\blu_f.hpp"
     };
     class BLU_G_F
     {
         displayName = "FIA (West)";
-        category = "vanilla";
+        category = "Vanilla";
         #include "loadouts\fia_f.hpp"
     };
 
     class OPF_F
     {
         displayName = "CSAT";
-        category = "vanilla";
+        category = "Vanilla";
         #include "loadouts\opf_f.hpp"
     };
     class OPF_G_F
     {
         displayName = "FIA (East)";
-        category = "vanilla";
+        category = "Vanilla";
         #include "loadouts\fia_f.hpp"
     };
     class IND_F
     {
         displayName = "AAF";
-        category = "vanilla";
+        category = "Vanilla";
         #include "loadouts\ind_f.hpp"
     };
     class IND_G_F
     {
         displayName = "FIA (Resistance)";
-        category = "vanilla";
+        category = "Vanilla";
         #include "loadouts\fia_f.hpp"
     };
     class CIV_F
     {
         displayName = "Altis Civilians";
-        category = "vanilla";
+        category = "Vanilla";
         #include "loadouts\civ_f.hpp"
     };
 };
