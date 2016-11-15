45c373c8d9466d9b7235c5d787f481e86faf7878
Sniperhid <Sniperhid@users.noreply.github.com>
2016-11-15 14:31:09 +0000

    Tabs to spaces

--- addons/common/display3DEN.hpp
+++ addons/common/display3DEN.hpp
@@ -35,42 +35,42 @@ class display3DEN
         class Toolbar : ctrlControlsGroupNoScrollbars{
             class Controls {
                 class Separator3: ctrlStatic
-				{
+                {
                     colorBackground[]={0,0,0,0.5};
-					x="33.5 * 	(	5 * (pixelW * pixelGrid * 	0.50))";
-					y="1 * (pixelH * pixelGrid * 	0.50)";
-					w="pixelW";
-					h="(	5 * (pixelH * pixelGrid * 	0.50))";
-				};
+                    x="33.5 *     (    5 * (pixelW * pixelGrid *     0.50))";
+                    y="1 * (pixelH * pixelGrid *     0.50)";
+                    w="pixelW";
+                    h="(    5 * (pixelH * pixelGrid *     0.50))";
+                };
                 class TMF_Toolbar_Controls : ctrlControlsGroupNoScrollbars {
                     idc=-1;
-					x="34.5 * (5 * (pixelW * pixelGrid * 	0.50))";
-					y="1 * (pixelH * pixelGrid * 	0.50)";
-					w="5 * 	(	5 * (pixelW * pixelGrid * 	0.50))";
-					h="(	5 * (pixelH * pixelGrid * 	0.50))";
-					class Controls
-					{
-						class Garrison: ctrlCheckboxToolbar
-						{
-							idc=-1;
-							onCheckedChanged=QUOTE(GVAR(Garrison) = !GVAR(Garrison;));
-							onLoad=QUOTE(GVAR(Garrison) = false);
-                            x="0 * 	(	5 * (pixelW * pixelGrid * 	0.50))";
-							y=0;
-							h="(	5 * (pixelH * pixelGrid * 	0.50))";
-							w="(	5 * (pixelW * pixelGrid * 	0.50))";
-							tooltip="Toggle garrison on drag";
-							textureChecked="\x\tmf\addons\common\UI\logo_tmf_small_toolbar_active_ca.paa";
-							textureUnchecked="\x\tmf\addons\common\UI\logo_tmf_small_ca.paa";
-							textureFocusedChecked="\x\tmf\addons\common\UI\logo_tmf_small_toolbar_active_ca.paa";
-							textureFocusedUnchecked="\x\tmf\addons\common\UI\logo_tmf_small_ca.paa";
-							textureHoverChecked="\x\tmf\addons\common\UI\logo_tmf_small_toolbar_active_ca.paa";
-							textureHoverUnchecked="\x\tmf\addons\common\UI\logo_tmf_small_ca.paa";
-							texturePressedChecked="\x\tmf\addons\common\UI\logo_tmf_small_toolbar_active_ca.paa";
-							texturePressedUnchecked="\x\tmf\addons\common\UI\logo_tmf_small_ca.paa";
-							textureDisabledChecked="\x\tmf\addons\common\UI\logo_tmf_small_toolbar_active_ca.paa";
-							textureDisabledUnchecked="\x\tmf\addons\common\UI\logo_tmf_small_ca.paa";
-						};
+                    x="34.5 * (5 * (pixelW * pixelGrid *     0.50))";
+                    y="1 * (pixelH * pixelGrid *     0.50)";
+                    w="5 *     (    5 * (pixelW * pixelGrid *     0.50))";
+                    h="(    5 * (pixelH * pixelGrid *     0.50))";
+                    class Controls
+                    {
+                        class Garrison: ctrlCheckboxToolbar
+                        {
+                            idc=-1;
+                            onCheckedChanged=QUOTE(GVAR(Garrison) = !GVAR(Garrison;));
+                            onLoad=QUOTE(GVAR(Garrison) = false);
+                            x="0 *     (    5 * (pixelW * pixelGrid *     0.50))";
+                            y=0;
+                            h="(    5 * (pixelH * pixelGrid *     0.50))";
+                            w="(    5 * (pixelW * pixelGrid *     0.50))";
+                            tooltip="Toggle garrison on drag";
+                            textureChecked="\x\tmf\addons\common\UI\logo_tmf_small_toolbar_active_ca.paa";
+                            textureUnchecked="\x\tmf\addons\common\UI\logo_tmf_small_ca.paa";
+                            textureFocusedChecked="\x\tmf\addons\common\UI\logo_tmf_small_toolbar_active_ca.paa";
+                            textureFocusedUnchecked="\x\tmf\addons\common\UI\logo_tmf_small_ca.paa";
+                            textureHoverChecked="\x\tmf\addons\common\UI\logo_tmf_small_toolbar_active_ca.paa";
+                            textureHoverUnchecked="\x\tmf\addons\common\UI\logo_tmf_small_ca.paa";
+                            texturePressedChecked="\x\tmf\addons\common\UI\logo_tmf_small_toolbar_active_ca.paa";
+                            texturePressedUnchecked="\x\tmf\addons\common\UI\logo_tmf_small_ca.paa";
+                            textureDisabledChecked="\x\tmf\addons\common\UI\logo_tmf_small_toolbar_active_ca.paa";
+                            textureDisabledUnchecked="\x\tmf\addons\common\UI\logo_tmf_small_ca.paa";
+                        };
                     };
                 };
             };
