7d23ab304233733ade1bae83322f42b19daa72ac
Sebastian Broberg <headswe92@gmail.com>
2016-11-21 19:02:27 +0100

    Removed tabs.

--- addons/spectator/config.cpp
+++ addons/spectator/config.cpp
@@ -40,46 +40,46 @@ class CfgRespawnTemplates
 
 class GVAR(EntityTag): RscControlsGroup
 {
-	idc = -1;
-	x = 0 * safezoneW + safezoneX;
-	y = 0 * safezoneH + safezoneY;
-	w = 0.05 * safezoneW;
-	h = 0.06 * safezoneH;
-	class controls
-	{
-		class GVAR(Icon): RscPicture
-		{
-			idc = 1;
-			text = "\A3\ui_f\data\map\markers\military\triangle_CA.paa";
-			x = 0 * safezoneW;
-			y = 0 * safezoneH;
-			w = 0.05 * safezoneW;
-			h = 0.02 * safezoneH;
-            style = 48 + 0x800 + 0x02; // picture + no stretch + center (not sure if works)
-			sizeEx = 0.1 * GUI_GRID_H;
-		};
-		class GVAR(Name): RscText
-		{
-			idc = 2;
-			text = "Head"; //--- ToDo: Localize;
-            style = 0x02; // single + center align
-            colorBackground[] = { 1, 1, 1, 0 };
-    		colorText[] = { 1, 1, 1, 1 };
-			x = 0.00 * safezoneW;
-			y = 0.02 * safezoneH;
-			w = 0.05 * safezoneW;
-			h = 0.02 * safezoneH;
-			sizeEx = 0.018 * safeZoneH;
-		};
-		class GVAR(Detail): GVAR(Name)
-		{
-			idc = 3;
-			text = "Alpha 1"; //--- ToDo: Localize;
-			x = 0.00 * safezoneW;
-			y = 0.04 * safezoneH;
-			w = 0.05 * safezoneW;
-			h = 0.02 * safezoneH;
-			sizeEx = 0.013 * safeZoneH;
-		};
-	};
+    idc = -1;
+    x = 0 * safezoneW + safezoneX;
+    y = 0 * safezoneH + safezoneY;
+    w = 0.05 * safezoneW;
+    h = 0.06 * safezoneH;
+    class controls
+    {
+        class GVAR(Icon): RscPicture
+        {
+            idc = 1;
+            text = "\A3\ui_f\data\map\markers\military\triangle_CA.paa";
+            x = 0 * safezoneW;
+            y = 0 * safezoneH;
+            w = 0.05 * safezoneW;
+            h = 0.02 * safezoneH;
+                    style = 48 + 0x800 + 0x02; // picture + no stretch + center (not sure if works)
+            sizeEx = 0.1 * GUI_GRID_H;
+        };
+        class GVAR(Name): RscText
+        {
+            idc = 2;
+            text = "Head"; //--- ToDo: Localize;
+                    style = 0x02; // single + center align
+                    colorBackground[] = { 1, 1, 1, 0 };
+            		colorText[] = { 1, 1, 1, 1 };
+            x = 0.00 * safezoneW;
+            y = 0.02 * safezoneH;
+            w = 0.05 * safezoneW;
+            h = 0.02 * safezoneH;
+            sizeEx = 0.018 * safeZoneH;
+        };
+        class GVAR(Detail): GVAR(Name)
+        {
+            idc = 3;
+            text = "Alpha 1"; //--- ToDo: Localize;
+            x = 0.00 * safezoneW;
+            y = 0.04 * safezoneH;
+            w = 0.05 * safezoneW;
+            h = 0.02 * safezoneH;
+            sizeEx = 0.013 * safeZoneH;
+        };
+    };
 };
