6efddf132a9be2ea0a2467e068e2972ff4ac088c
Sebastian Broberg <headswe92@gmail.com>
2017-01-09 17:44:42 +0100

    Some more clean up.

--- addons/spectator/config.cpp
+++ addons/spectator/config.cpp
@@ -38,108 +38,18 @@ class CfgRespawnTemplates
 
 
 
-class GVAR(EntityTag): ctrlControlsGroupNoScrollbars
-{
-    idc = -1;
-    x = 0 * safezoneW + safezoneX;
-    y = 0 * safezoneH + safezoneY;
-    w = 0.08 * safezoneW;
-    h = 0.06 * safezoneH;
-    onMouseZChanged = "[""MouseZChanged"",_this] call tmf_spectator_fnc_mouseHandler";
-    onMouseButtonDown = "[""MouseButtonDown"",_this] call tmf_spectator_fnc_mouseHandler";
-    onMouseButtonUp = "[""MouseButtonUp"",_this] call tmf_spectator_fnc_mouseHandler";
-    class controls
-    {
-        class GVAR(back) : IGUIBACK {
-            idc = 4;
-            x = 0.3 * safezoneW;
-            y = 0;
-            w = 0.05 * safezoneW;
-            h = 0.06 * safezoneH;
-            colorBackground[] = {0,0,0,0.0};
-        }
-        class GVAR(Icon): RscPicture
-        {
-            idc = 1;
-            text = "\A3\ui_f\data\map\markers\military\triangle_CA.paa";
-            x = 0;
-            y = 0;
-            w = 0.08 * safezoneW;
-            h = 0.02 * safezoneH;
-            style = 48 + 2048 + 0x02; // picture + no stretch + center (not sure if works) + 0x800
-            sizeEx = 1 * safeZoneH;
-        };
-        class GVAR(Name): RscText
-        {
-            idc = 2;
-            text = ""; //--- ToDo: Localize;
-            style = 0x02; // single + center align
-            colorBackground[] = { 1, 1, 1, 0 };
-            colorText[] = { 1, 1, 1, 1 };
-            x = 0;
-            y = 0.02 * safezoneH;
-            w = 0.08 * safezoneW;
-            h = 0.02 * safezoneH;
-            sizeEx = 0.018 * safeZoneH;
-            shadow = 2;
-        };
-        class GVAR(Detail): GVAR(Name)
-        {
-            idc = 3;
-            text = ""; //--- ToDo: Localize;
-            x = 0;
-            y = 0.04 * safezoneH;
-            w = 0.08 * safezoneW;
-            h = 0.02 * safezoneH;
-            sizeEx = 0.013 * safeZoneH;
-        };
+#include "tags.hpp";
+
+
+
+class CfgVehicles {
+    class VirtualMan_F;
+    class GVAR(unit) : VirtualMan_F {
+        author = ADDON;
+        scope = 1;
+        scopeCurator = 1,
+        scopeArsenal = 1;
+        delete ACE_SelfActions;
+        delete ACE_Actions;
     };
 };
-class GVAR(GroupTag) : GVAR(EntityTag) {
-    class controls
-        {
-            class GVAR(back) : IGUIBACK {
-                idc = 4;
-                x = 0.3 * safezoneW;
-                y = 0;
-                w = 0.05 * safezoneW;
-                h = 0.06 * safezoneH;
-                colorBackground[] = {0,0,0,0.0};
-            }
-            class GVAR(Icon): RscPicture
-            {
-                idc = 1;
-                text = "\A3\ui_f\data\map\markers\military\triangle_CA.paa";
-                x = 0;
-                y = 0;
-                w = 0.08 * safezoneW;
-                h = 0.035 * safezoneH;
-                style = 48 + 2048 + 0x02; // picture + no stretch + center (not sure if works) + 0x800
-                sizeEx = 1 * safeZoneH;
-            };
-            class GVAR(Name): RscText
-            {
-                idc = 2;
-                text = ""; //--- ToDo: Localize;
-                style = 0x02; // single + center align
-                colorBackground[] = { 1, 1, 1, 0 };
-                colorText[] = { 1, 1, 1, 1 };
-                x = 0;
-                y = 0.04 * safezoneH;
-                w = 0.08 * safezoneW;
-                h = 0.02 * safezoneH;
-                sizeEx = 0.018 * safeZoneH;
-                shadow = 2;
-            };
-            class GVAR(Detail): GVAR(Name)
-            {
-                idc = 3;
-                text = ""; //--- ToDo: Localize;
-                x = 0;
-                y = 0.08 * safezoneH;
-                w = 0.08 * safezoneW;
-                h = 0.02 * safezoneH;
-                sizeEx = 0.013 * safeZoneH;
-            };
-        };
-}
--- addons/spectator/functions/fn_onLoad.sqf
+++ addons/spectator/functions/fn_onLoad.sqf
@@ -3,10 +3,7 @@ params ["_display"];
 #include "\x\tmf\addons\spectator\script_component.hpp"
 
 
-
-
 with uiNamespace do {
-
     GVAR(display) = _display;
     GVAR(unitlabel) = _display displayCtrl IDC_SPECTATOR_TMF_SPECTATOR_UNITLABEL;
     GVAR(unitlist) = _display displayCtrl IDC_SPECTATOR_TMF_SPECTATOR_UNITLIST;
@@ -19,28 +16,25 @@ with uiNamespace do {
     GVAR(view) = _display displayCtrl IDC_SPECTATOR_TMF_SPECTATOR_VIEW;
     GVAR(mute) = _display displayCtrl IDC_SPECTATOR_TMF_SPECTATOR_MUTE;
     GVAR(map) = _display displayCtrl IDC_SPECTATOR_TMF_SPECTATOR_MAP;
-
-    GVAR(compass) = _display displayCtrl IDC_SPECTATOR_TMF_SPECTATOR_COMPASS;
-    GVAR(compassL) = _display displayCtrl IDC_SPECTATOR_TMF_SPECTATOR_COMPASSLEFT;
-    GVAR(compassR) = _display displayCtrl IDC_SPECTATOR_TMF_SPECTATOR_COMPASSRight;
+    GVAR(compass) = [_display displayCtrl IDC_SPECTATOR_TMF_SPECTATOR_COMPASS,_display displayCtrl IDC_SPECTATOR_TMF_SPECTATOR_COMPASSLEFT,_display displayCtrl IDC_SPECTATOR_TMF_SPECTATOR_COMPASSRight];
+    GVAR(compass) = 
+    GVAR(compassL) = ;
+    GVAR(compassR) = ;
 
     _labelParent = _display displayCtrl 2300;
     GVAR(labels) = [
-    _labelParent controlsGroupCtrl 6
-    ,_labelParent controlsGroupCtrl 7
-    ,_labelParent controlsGroupCtrl 8
-    ,_labelParent controlsGroupCtrl 9
-    ,_labelParent controlsGroupCtrl 10
-    ,_labelParent controlsGroupCtrl 11
+        _labelParent controlsGroupCtrl 6,
+        _labelParent controlsGroupCtrl 7,
+        _labelParent controlsGroupCtrl 8,
+        _labelParent controlsGroupCtrl 9,
+        _labelParent controlsGroupCtrl 10,
+        _labelParent controlsGroupCtrl 11
     ];
-
 
     GVAR(map) ctrlShow (missionNamespace getVariable [QGVAR(showMap),false]);
     GVAR(map) mapCenterOnCamera false;
 
 };
-
-
 
 
 if(!getMissionConfigValue ["TMF_Spectator_AllSides",true]) then {
@@ -59,7 +53,6 @@ _allowedModes = [getMissionConfigValue ["TMF_Spectator_AllowFollowCam",true],get
 
 // if ACRE2 is enabled, enable the mute button
 if (isClass(configFile >> "CfgPatches" >> "acre_main")) then {
-
     [true] call acre_api_fnc_setSpectator;
     _data = ["ACRE2","HeadSet"] call CBA_fnc_getKeybind;
     GVAR(mute_key) = (_data select 5) select 0;
@@ -73,10 +66,8 @@ if (isClass(configFile >> "CfgPatches" >> "acre_main")) then {
         } forEach tmf_acre2_languagesTable;
         _languages call acre_api_fnc_babelSetSpokenLanguages;
     };
-
 }
-else // else remove it
-{
+else { // else remove it
     with uiNamespace do {
         GVAR(mute) ctrlShow false; // hide mute button
     };
