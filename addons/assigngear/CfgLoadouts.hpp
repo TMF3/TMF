6cac97e65d8cf7ea2ac808d8ca9cd75c6510d06d
Snippers <Sniperhid@users.noreply.github.com>
2017-02-09 20:18:10 +0000

    Assign Gear - In eden add tooltips for factions and roles. (#87)

--- addons/assigngear/CfgLoadouts.hpp
+++ addons/assigngear/CfgLoadouts.hpp
@@ -4,12 +4,14 @@ class cfgLoadouts
     {
         displayName = "NATO";
         category = "Vanilla";
+        tooltip = "Uses standard NATO equipment in vanilla Arma 3.";
         #include "loadouts\blu_f.hpp"
     };
     class BLU_G_F
     {
         displayName = "FIA (West)";
         category = "Vanilla";
+        tooltip = "Uses standard FIA equipment in vanilla Arma 3.";
         #include "loadouts\fia_f.hpp"
     };
 
@@ -17,30 +19,35 @@ class cfgLoadouts
     {
         displayName = "CSAT";
         category = "Vanilla";
+        tooltip = "Uses standard CSAT equipment in vanilla Arma 3.";
         #include "loadouts\opf_f.hpp"
     };
     class OPF_G_F
     {
         displayName = "FIA (East)";
         category = "Vanilla";
+        tooltip = "Uses standard FIA equipment in vanilla Arma 3.";
         #include "loadouts\fia_f.hpp"
     };
     class IND_F
     {
         displayName = "AAF";
         category = "Vanilla";
+        tooltip = "Uses standard AAF equipment in vanilla Arma 3.";
         #include "loadouts\ind_f.hpp"
     };
     class IND_G_F
     {
         displayName = "FIA (Resistance)";
         category = "Vanilla";
+        tooltip = "Uses standard FIA equipment in vanilla Arma 3.";
         #include "loadouts\fia_f.hpp"
     };
     class CIV_F
     {
         displayName = "Altis Civilians";
         category = "Vanilla";
+        tooltip = "Uses civilian uniforms from vanilla Arma 3.";
         #include "loadouts\civ_f.hpp"
     };
 };
