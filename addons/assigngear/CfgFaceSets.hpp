700605ce6f8f20a44266f4bb68800caf8a61e473
Bear <beaar@users.noreply.github.com>
2018-10-10 00:22:27 +0200

    Define facesets (#244)
    * Define facesets
    
    * Facesets to loadouts

--- addons/assigngear/CfgFaceSets.hpp
+++ addons/assigngear/CfgFaceSets.hpp
@@ -1,11 +1,142 @@
 class GVAR(facesets) {
     class african {
-        // Classname
         class AfricanHead_01 {
-            probability = 0.1;
+            /*probability = 0.1;*/
         };
-        class AfricanHead_02 {
-            probability = 0.2;
-        };
+        class AfricanHead_02 {};
+        class AfricanHead_03 {};
+        class Barklem {};
+        class asczHead_foley_A3 {};
+        class asczHead_grey_A3 {};
+        class asczHead_josephe_A3 {};
+        class asczHead_murphy_A3 {};
+    };
+    
+    class asian {
+        class AsianHead_A3_01 {};
+        class AsianHead_A3_02 {};
+        class AsianHead_A3_03 {};
+        class AsianHead_A3_04 {};
+        class AsianHead_A3_05 {};
+        class AsianHead_A3_06 {};
+        class AsianHead_A3_07 {};
+        class asczHead_idoka_A3 {};
+        class asczHead_xong_A3 {};
+    };
+    
+    class greek {
+        class GreekHead_A3_01 {};
+        class GreekHead_A3_02 {};
+        class GreekHead_A3_03 {};
+        class GreekHead_A3_04 {};
+        class GreekHead_A3_06 {};
+        class GreekHead_A3_08 {};
+        class GreekHead_A3_09 {};
+        class GreekHead_A3_10 {};
+        class GreekHead_A3_11 {};
+        class GreekHead_A3_12 {};
+        class GreekHead_A3_13 {};
+        class GreekHead_A3_14 {};
+        class Ioannou {};
+        class Kerry_B2_F {};
+        class Mavros {};
+        class asczHead_boredslav_A3 {};
+        class asczHead_brown_A3 {};
+        class asczHead_hammond_A3 {};
+        class asczHead_novotny_A3 {};
+        class asczHead_obacki_A3 {};
+        class asczHead_olson_A3 {};
+        class asczHead_picleson_A3 {};
+        class asczHead_plumber_A3 {};
+        class asczHead_price_A3 {};
+        class asczHead_redfield_A3 {};
+        class asczHead_slug_A3 {};
+        class asczHead_snek_A3 {};
+        class asczHead_taurus_A3 {};
+        class asczHead_woods_A3 {};
+    };
+    
+    class persian {
+        class Mavros {};
+        class O_Colonel {};
+        class PersianHead_A3_01 {};
+        class PersianHead_A3_02 {};
+        class PersianHead_A3_03 {};
+        class PersianHead_A3_04 {};
+        class asczHead_boomlakhaoo_A3 {};
+    };
+    
+    class tanoan {
+        class TanoanBossHead {};
+        class TanoanHead_A3_01 {};
+        class TanoanHead_A3_02 {};
+        class TanoanHead_A3_03 {};
+        class TanoanHead_A3_04 {};
+        class TanoanHead_A3_05 {};
+        class TanoanHead_A3_06 {};
+        class TanoanHead_A3_07 {};
+        class TanoanHead_A3_08 {};
+        class asczHead_boomlakhaoo_A3 {};
+        class asczHead_grey_A3 {};
+    };
+    
+    class caucasian {
+        class GreekHead_A3_03 {};
+        class GreekHead_A3_04 {};
+        class GreekHead_A3_08 {};
+        class GreekHead_A3_09 {};
+        class GreekHead_A3_10 {};
+        class GreekHead_A3_11 {};
+        class GreekHead_A3_12 {};
+        class GreekHead_A3_13 {};
+        class GreekHead_A3_14 {};
+        class Kerry {};
+        class Sturrock {};
+        class WhiteHead_01 {};
+        class WhiteHead_02 {};
+        class WhiteHead_03 {};
+        class WhiteHead_04 {};
+        class WhiteHead_05 {};
+        class WhiteHead_06 {};
+        class WhiteHead_07 {};
+        class WhiteHead_08 {};
+        class WhiteHead_09 {};
+        class WhiteHead_10 {};
+        class WhiteHead_11 {};
+        class WhiteHead_12 {};
+        class WhiteHead_13 {};
+        class WhiteHead_14 {};
+        class WhiteHead_15 {};
+        class WhiteHead_16 {};
+        class WhiteHead_17 {};
+        class WhiteHead_18 {};
+        class WhiteHead_19 {};
+        class WhiteHead_20 {};
+        class WhiteHead_21 {};
+        class WhiteHead_22 {};
+        class WhiteHead_23 {};
+        class asczHead_austin_A3 {};
+        class asczHead_beardy_A3 {};
+        class asczHead_boredslav_A3 {};
+        class asczHead_brown_A3 {};
+        class asczHead_hammond_A3 {};
+        class asczHead_johnhoot_A3 {};
+        class asczHead_logan_A3 {};
+        class asczHead_mctavish_A3 {};
+        class asczHead_muller_A3 {};
+        class asczHead_neumann_A3 {};
+        class asczHead_nevim_A3 {};
+        class asczHead_olson_A3 {};
+        class asczHead_picleson_A3 {};
+        class asczHead_plumber_A3 {};
+        class asczHead_price_A3 {};
+        class asczHead_slug_A3 {};
+        class asczHead_sokolov_A3 {};
+        class asczHead_spanel_A3 {};
+        class asczHead_stick_A3 {};
+        class asczHead_taurus_A3 {};
+        class asczHead_troska_A3 {};
+        class asczHead_vlk_A3 {};
+        class asczHead_voodoo_A3 {};
     };
 };
