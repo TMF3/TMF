ca032df2726dd96bb72b260986822a094cdeb9fd
Bear <nipronz@gmail.com>
2019-11-14 13:36:57 +0100

    Fix tabs

--- addons/adminmenu/gui/adminMenu.hpp
+++ addons/adminmenu/gui/adminMenu.hpp
@@ -305,24 +305,24 @@ class GVAR(spectatorControlUnitDialog) {
     movingEnable = 0;
     class Controls {};
     class ControlsBackground {
-		class Title: RscTitle {
+        class Title: RscTitle {
             idc = -1;
-			text = "Select Unit in Vehicle";
+            text = "Select Unit in Vehicle";
             x = "1 * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX + (safezoneW - ((safezoneW / safezoneH) min 1.2))/2)";
             y = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + (safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))/2)";
             w = "19 * (((safezoneW / safezoneH) min 1.2) / 40)";
             h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
-			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.13])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.54])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.21])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.8])"};
-			shadow = 1;
+            colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.13])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.54])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.21])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.8])"};
+            shadow = 1;
             style = 0;
-		};
+        };
         class Background: RscText {
             onLoad = QUOTE(_this call FUNC(remoteControl_dialog));
             x = "1 * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX + (safezoneW - ((safezoneW / safezoneH) min 1.2))/2)";
             y = "2.1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + (safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))/2)";
             w = "19 * (((safezoneW / safezoneH) min 1.2) / 40)";
             h = "16 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
-			colorBackground[] = {0,0,0,0.7};
-		};
+            colorBackground[] = {0,0,0,0.7};
+        };
     };
 };
