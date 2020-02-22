f119e2407133928532a61ac29ee675a2d5a856a4
Freddo3000 <f.falk@protonmail.com>
2020-02-22 15:53:21 +0100

    Tabs to spaces

--- addons/assigngear/gui/gearSelector.hpp
+++ addons/assigngear/gui/gearSelector.hpp
@@ -1,67 +1,67 @@
 class GVAR(RscGearSelector) : RscStandardDisplay {
-	idd = IDD_RSCGEARSELECTOR;
-	onLoad = QUOTE(uiNamespace setVariable [ARR_2(QQGVAR(gear_display), _this select 0)]; _this call FUNC(gui_gearSelector_init););
-	onUnload = QUOTE(uiNamespace setVariable [ARR_2(QQGVAR(gear_display), displayNull)];);
+    idd = IDD_RSCGEARSELECTOR;
+    onLoad = QUOTE(uiNamespace setVariable [ARR_2(QQGVAR(gear_display), _this select 0)]; _this call FUNC(gui_gearSelector_init););
+    onUnload = QUOTE(uiNamespace setVariable [ARR_2(QQGVAR(gear_display), displayNull)];);
 
-	class controls {
+    class controls {
         class Title : RscTitle {
-			text = "TMF Loadout Jukebox";
+            text = "TMF Loadout Jukebox";
 
-			x = CENTER_X - 24 * GRID_W;
+            x = CENTER_X - 24 * GRID_W;
             y = CENTER_Y - GRID_H * 15;
             w = 50 * GRID_W;
             h = SIZE_M * GRID_H;
             sizeEx = SIZEEX_PURISTA(SIZEEX_M);
-		};
+        };
         class TitleIcon : RscPicture {
-			text = QPATHTOEF(common,UI\logo_tmf_small_ca.paa);
+            text = QPATHTOEF(common,UI\logo_tmf_small_ca.paa);
 
-			x = CENTER_X - 30 * GRID_W;
+            x = CENTER_X - 30 * GRID_W;
             y = CENTER_Y - GRID_H * 15;
             w = SIZE_M * GRID_H;
             h = SIZE_M * GRID_H;
 
-		};
-		class CategoryLabel : RscText {
-			text = "Category:";
+        };
+        class CategoryLabel : RscText {
+            text = "Category:";
 
-			x = CENTER_X - 30 * GRID_W;
-			y = CENTER_Y - GRID_H * 9;
-			w = 60 * GRID_W;
-			h = SIZE_S * GRID_H;
+            x = CENTER_X - 30 * GRID_W;
+            y = CENTER_Y - GRID_H * 9;
+            w = 60 * GRID_W;
+            h = SIZE_S * GRID_H;
             sizeEx = SIZEEX_PURISTA(SIZEEX_S);
-		};
-		class Category : RscCombo {
-			idc = IDC_RSCGEARSELECTOR_CATEGORY;
+        };
+        class Category : RscCombo {
+            idc = IDC_RSCGEARSELECTOR_CATEGORY;
 
-			x = CENTER_X - 28 * GRID_W;
+            x = CENTER_X - 28 * GRID_W;
             y = CENTER_Y - GRID_H * 5;
-			w = 56 * GRID_W;
-			h = SIZE_S * GRID_H;
+            w = 56 * GRID_W;
+            h = SIZE_S * GRID_H;
             sizeEx = SIZEEX_PURISTA(SIZEEX_S);
 
-			onLBSelChanged = QUOTE( \
+            onLBSelChanged = QUOTE( \
                 params [ARR_2('_ctrl', '_selectedIndex')]; \
                 [ARR_2(ctrlParent _ctrl, _selectedIndex)] call FUNC(gui_gearSelector_loadFactions); \
                 false \
             );
-		};
-		class FactionLabel : RscText {
-			text = "Faction:";
+        };
+        class FactionLabel : RscText {
+            text = "Faction:";
 
-			x = CENTER_X - 30 * GRID_W;
-			y = CENTER_Y - GRID_H * 1;
+            x = CENTER_X - 30 * GRID_W;
+            y = CENTER_Y - GRID_H * 1;
             w = 60 * GRID_W;
-			h = SIZE_S * GRID_H;
+            h = SIZE_S * GRID_H;
             sizeEx = SIZEEX_PURISTA(SIZEEX_S);
-		};
-		class Faction : RscCombo {
-			idc = IDC_RSCGEARSELECTOR_FACTION;
+        };
+        class Faction : RscCombo {
+            idc = IDC_RSCGEARSELECTOR_FACTION;
 
-			x = CENTER_X - 28 * GRID_W;
+            x = CENTER_X - 28 * GRID_W;
             y = CENTER_Y + GRID_H * 3;
-			w = 56 * GRID_W;
-			h = SIZE_S * GRID_H;
+            w = 56 * GRID_W;
+            h = SIZE_S * GRID_H;
             sizeEx = SIZEEX_PURISTA(SIZEEX_S);
 
             onLBSelChanged = QUOTE( \
@@ -69,79 +69,79 @@ class GVAR(RscGearSelector) : RscStandardDisplay {
                 [ARR_2(ctrlParent _ctrl, _selectedIndex)] call FUNC(gui_gearSelector_loadRoles); \
                 false \
             );
-		};
-		class RoleLabel : RscText{
-			text = "Role:";
+        };
+        class RoleLabel : RscText{
+            text = "Role:";
 
-			x = CENTER_X - 30 * GRID_W;
-			y = CENTER_Y + GRID_H * 7;
+            x = CENTER_X - 30 * GRID_W;
+            y = CENTER_Y + GRID_H * 7;
             w = 60 * GRID_W;
-			h = SIZE_S * GRID_H;
+            h = SIZE_S * GRID_H;
             sizeEx = SIZEEX_PURISTA(SIZEEX_S);
-		};
-		class Role : RscCombo{
-			idc = IDC_RSCGEARSELECTOR_ROLE;
+        };
+        class Role : RscCombo{
+            idc = IDC_RSCGEARSELECTOR_ROLE;
 
-			x = CENTER_X - 28 * GRID_W;
+            x = CENTER_X - 28 * GRID_W;
             y = CENTER_Y + GRID_H * 11;
-			w = 56 * GRID_W;
-			h = SIZE_S * GRID_H;
+            w = 56 * GRID_W;
+            h = SIZE_S * GRID_H;
             sizeEx = SIZEEX_PURISTA(SIZEEX_S);
-		};
+        };
 
-		class ButtonCancel : RscButtonMenuCancel {
-			x = CENTER_X - 30 * GRID_W;
-			y = CENTER_Y + GRID_H * 17;
+        class ButtonCancel : RscButtonMenuCancel {
+            x = CENTER_X - 30 * GRID_W;
+            y = CENTER_Y + GRID_H * 17;
             w = (59 / 3) * GRID_W;
-			h = SIZE_M * GRID_H;
+            h = SIZE_M * GRID_H;
             sizeEx = SIZEEX_PURISTA(SIZEEX_S);
-		};
-		class ButtonRandom : RscButtonMenu {
-			text = "Random";
+        };
+        class ButtonRandom : RscButtonMenu {
+            text = "Random";
             idc = IDC_RSCGEARSELECTOR_RANDOM;
 
-			onButtonClick = QUOTE( \
+            onButtonClick = QUOTE( \
                 params ['_ctrl']; \
                 [(ctrlParent _ctrl)] call FUNC(gui_gearSelector_random); \
             );
 
-			x = CENTER_X - ((59 / 3) * GRID_W) / 2;
-			y = CENTER_Y + GRID_H * 17;
-			w = (59 / 3) * GRID_W;
-			h = SIZE_M * GRID_H;
+            x = CENTER_X - ((59 / 3) * GRID_W) / 2;
+            y = CENTER_Y + GRID_H * 17;
+            w = (59 / 3) * GRID_W;
+            h = SIZE_M * GRID_H;
             sizeEx = SIZEEX_PURISTA(SIZEEX_S);
-		};
-		class ButtonOK : RscButtonMenuOK {
+        };
+        class ButtonOK : RscButtonMenuOK {
             idc = IDC_RSCGEARSELECTOR_SUBMIT;
 
-			onButtonClick = QUOTE( \
+            onButtonClick = QUOTE( \
                 params ['_ctrl']; \
                 [(ctrlParent _ctrl)] call FUNC(gui_gearSelector_submit); \
             );
 
-			x = CENTER_X + (GRID_W * 0.5) + ((59 / 3) * GRID_W) / 2;
-			y = CENTER_Y + GRID_H * 17;
-			w = (59 / 3) * GRID_W;
-			h = SIZE_M * GRID_H;
+            x = CENTER_X + (GRID_W * 0.5) + ((59 / 3) * GRID_W) / 2;
+            y = CENTER_Y + GRID_H * 17;
+            w = (59 / 3) * GRID_W;
+            h = SIZE_M * GRID_H;
             sizeEx = SIZEEX_PURISTA(SIZEEX_S);
-		};
-	};
-	class controlsBackground {
+        };
+    };
+    class controlsBackground {
         class TitleBackground : RscBackgroundGUITop {
             x = CENTER_X - 30 * GRID_W;
             y = CENTER_Y - GRID_H * 15;
             w = 60 * GRID_W;
             h = SIZE_M * GRID_H;
 
-			colorBackground[] = {COLOR_ACTIVE_RGBA};
+            colorBackground[] = {COLOR_ACTIVE_RGBA};
 
             backgroundType = 1;
-		};
-		class Background : RscBackgroundGUI {
+        };
+        class Background : RscBackgroundGUI {
             x = CENTER_X - 30 * GRID_W;
             y = CENTER_Y - GRID_H * 9.5;
             w = 60 * GRID_W;
             h = 26 * GRID_H;
-		};
-	};
+        };
+    };
 };
