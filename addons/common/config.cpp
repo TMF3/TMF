1485da211b061f17d226da67431ac47461298e92
Freddo3000 <f.falk@protonmail.com>
2020-08-09 01:55:45 +0200

    Store loadscreens in parsingNamespace

--- addons/common/config.cpp
+++ addons/common/config.cpp
@@ -19,6 +19,13 @@ class cfgPatches
 #include "CfgVehicles.hpp"
 #include "display3DEN.hpp"
 
+// Register loadscreens to parsingNamespace
+__EXEC(                                                                                      \
+    _arr = [];                                                                               \
+    for "_i" from 1 to 10 do { _arr pushBack format [QPATHTOF(UI\loadscreens\%1.jpg),_i]; }; \
+    GVAR(loadscreens) = _arr;                                                                \
+)
+
 class TMF_autotest {
     class GVAR(groupNamesSlottingScreen) {
         code = QUOTE([] call FUNC(testGroupsSlottingScreen));
