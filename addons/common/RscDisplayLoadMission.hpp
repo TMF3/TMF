a4f2fd255eb6ee5582a2d5247b7d2ab393f9a82d
Freddo3000 <f.falk@protonmail.com>
2020-08-09 23:12:54 +0200

    Add TMF watermark to loading screen
    Invalidates #379

---
+++ addons/common/RscDisplayLoadMission.hpp
@@ -0,0 +1,45 @@
+class RscStandardDisplay;
+class RscControlsGroupNoScrollbars;
+
+// Display TMF logo on supported missions
+class RscDisplayLoadMission: RscStandardDisplay {
+	class Controls {
+		class Mission: RscControlsGroupNoScrollbars {
+			class controls {
+                class MissionPicture;
+                class GVARMAIN(logo): MissionPicture {
+                    text = QPATHTOF(UI\LoadingImageTemplate.paa);
+                    idc = 108;
+                    onLoad = QUOTE(with uiNamespace do {                   \
+                        params ['_ctrl'];                                       \
+                        _ctrl ctrlShow false;                                   \
+                        if ([[ARR_3(1,1,1)]] call FUNC(checkTMFVersion)) then { \
+                            _ctrl ctrlShow true;                                \
+                        };                                                      \
+                    });
+                };
+            };
+		};
+	};
+};
+
+class RscDisplayNotFreeze: RscStandardDisplay {
+	class Controls {
+		class Mission: RscControlsGroupNoScrollbars {
+			class controls {
+                class MissionPicture;
+                class GVARMAIN(logo): MissionPicture {
+                    text = QPATHTOF(UI\LoadingImageTemplate.paa);
+                    idc = 108;
+                    onLoad = QUOTE(with uiNamespace do {                   \
+                        params ['_ctrl'];                                       \
+                        _ctrl ctrlShow false;                                   \
+                        if ([[ARR_3(1,1,1)]] call FUNC(checkTMFVersion)) then { \
+                            _ctrl ctrlShow true;                                \
+                        };                                                      \
+                    });
+                };
+            };
+		};
+	};
+};
